12207ae1165aa0272f2b000d2c172fe8d2eb7e53
Dao Heng Liu <daohengliu@gmail.com>
2024-03-14 09:20:32 +0000

    remove update manager

--- osu.Game/Overlays/Settings/Sections/General/UpdateSettings.cs
+++ osu.Game/Overlays/Settings/Sections/General/UpdateSettings.cs
@@ -1,29 +1,16 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Threading.Tasks;
 using osu.Framework.Allocation;
-using osu.Framework.Extensions;
-using osu.Framework.Graphics.Sprites;
 using osu.Framework.Localisation;
 using osu.Game.Configuration;
 using osu.Game.Localisation;
-using osu.Game.Overlays.Notifications;
-using osu.Game.Updater;
 
 namespace osu.Game.Overlays.Settings.Sections.General
 {
     public partial class UpdateSettings : SettingsSubsection
     {
         protected override LocalisableString Header => GeneralSettingsStrings.UpdateHeader;
-
-        private SettingsButton checkForUpdatesButton = null!;
-
-        [Resolved]
-        private UpdateManager? updateManager { get; set; }
-
-        [Resolved]
-        private INotificationOverlay? notifications { get; set; }
 
         [BackgroundDependencyLoader]
         private void load(OsuConfigManager config, OsuGame? game)
@@ -33,31 +20,6 @@ namespace osu.Game.Overlays.Settings.Sections.General
                 LabelText = GeneralSettingsStrings.ReleaseStream,
                 Current = config.GetBindable<ReleaseStream>(OsuSetting.ReleaseStream),
             });
-
-            if (updateManager?.CanCheckForUpdate == true)
-            {
-                Add(checkForUpdatesButton = new SettingsButton
-                {
-                    Text = GeneralSettingsStrings.CheckUpdate,
-                    Action = () =>
-                    {
-                        checkForUpdatesButton.Enabled.Value = false;
-                        Task.Run(updateManager.CheckForUpdateAsync).ContinueWith(task => Schedule(() =>
-                        {
-                            if (!task.GetResultSafely())
-                            {
-                                notifications?.Post(new SimpleNotification
-                                {
-                                    Text = GeneralSettingsStrings.RunningLatestRelease(game!.Version),
-                                    Icon = FontAwesome.Solid.CheckCircle,
-                                });
-                            }
-
-                            checkForUpdatesButton.Enabled.Value = true;
-                        }));
-                    }
-                });
-            }
         }
     }
 }
